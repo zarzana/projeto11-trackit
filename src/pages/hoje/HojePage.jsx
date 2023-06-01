191beef8491394b2d4cf5c27878414c1ce342129
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-06-01 16:05:18 -0300

    Refactored typo.

--- src/pages/hoje/HojePage.jsx
+++ src/pages/hoje/HojePage.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import NavBar from "../../components/NavBar";
 import StatusBar from "../../components/StatusBar";
-import Task from "./Tank";
+import Task from "./Task";
 
 function HojePage() {
 
